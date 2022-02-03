9e23910d958cc2d4149bb5e98bd814810c2c382f
igncarmona <98292399+igncarmona@users.noreply.github.com>
2022-02-03 13:58:57 -0400

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -3,12 +3,7 @@ const fs = require("fs");
 
 const html = NotionPageToHtml.convert(
   "https://sulfuric-canoe-a76.notion.site/19b44c6e26c543e58db57c0baacd8334?v=8b0c647f68914499a02cb283ef690cdb"
-).then((result) => createHTMLFile(result, "index.html"));
+).then((result) => document.queryselector('html') =result ;
 
-function createHTMLFile(content, fileName) {
-  const html = content.html;
-  var stream = fs.createWriteStream(fileName);
-  stream.once("open", function (fd) {
-    stream.end(html);
-  });
+
 }
