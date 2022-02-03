bd5792fb76e176c6a329418398fcdf0d77c3d5c5
igncarmona <98292399+igncarmona@users.noreply.github.com>
2022-02-03 14:06:30 -0400

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -2,15 +2,6 @@ const NotionPageToHtml = require("notion-page-to-html");
 const fs = require("fs");
 
 const html = NotionPageToHtml.convert(
-  "
-).then((result) => document.queryselector('html') =result ;
-
-
-}
-const NotionPageToHtml = require("notion-page-to-html");
-const fs = require("fs");
-
-const html = NotionPageToHtml.convert(
   "https://sulfuric-canoe-a76.notion.site/19b44c6e26c543e58db57c0baacd8334?v=8b0c647f68914499a02cb283ef690cdb"
 ).then((result) => createHTMLFile(result, "index.html"));
 
