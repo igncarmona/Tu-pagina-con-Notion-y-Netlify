abbc88684421eea3f797fd70e453578b7fee2f98
igncarmona <98292399+igncarmona@users.noreply.github.com>
2022-02-03 14:06:06 -0400

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -2,8 +2,22 @@ const NotionPageToHtml = require("notion-page-to-html");
 const fs = require("fs");
 
 const html = NotionPageToHtml.convert(
-  "https://sulfuric-canoe-a76.notion.site/19b44c6e26c543e58db57c0baacd8334?v=8b0c647f68914499a02cb283ef690cdb"
+  "
 ).then((result) => document.queryselector('html') =result ;
 
 
+}
+const NotionPageToHtml = require("notion-page-to-html");
+const fs = require("fs");
+
+const html = NotionPageToHtml.convert(
+  "https://sulfuric-canoe-a76.notion.site/19b44c6e26c543e58db57c0baacd8334?v=8b0c647f68914499a02cb283ef690cdb"
+).then((result) => createHTMLFile(result, "index.html"));
+
+function createHTMLFile(content, fileName) {
+  const html = content.html;
+  var stream = fs.createWriteStream(fileName);
+  stream.once("open", function (fd) {
+    stream.end(html);
+  });
 }
