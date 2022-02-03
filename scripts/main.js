b7591c84d87b66a6a1f6f4db1c3f72063e1a3ec9
igncarmona <98292399+igncarmona@users.noreply.github.com>
2022-02-03 13:37:40 -0400

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -2,7 +2,7 @@ const NotionPageToHtml = require("notion-page-to-html");
 const fs = require("fs");
 
 const html = NotionPageToHtml.convert(
-  "https://www.notion.so/dianait/Tu-pagina-web-con-Notion-y-Netlify-cfe8257a1cac405fa575c534a58e9437"
+  "https://sulfuric-canoe-a76.notion.site/19b44c6e26c543e58db57c0baacd8334?v=8b0c647f68914499a02cb283ef690cdb"
 ).then((result) => createHTMLFile(result, "index.html"));
 
 function createHTMLFile(content, fileName) {
